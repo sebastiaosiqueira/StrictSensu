cbe4a231d4fd76ee1fd7b8556315d35e5ecee4b3
Sebastiao siqueira <sebastiao.valfrides@gmail.com>
2026-06-12 09:15:49 -0400

    Grade

--- StrictSensu/obj/Debug/net9.0/StrictSensu.AssemblyInfo.cs
+++ StrictSensu/obj/Debug/net9.0/StrictSensu.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StrictSensu")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99975e05d063c65d1bde070cb0e285de07808649")]
 [assembly: System.Reflection.AssemblyProductAttribute("StrictSensu")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StrictSensu")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
