b0b9ba1974e05e2e9e8c43f33b7e11ffd771b274
Sebastiao siqueira <sebastiao.valfrides@gmail.com>
2026-06-22 13:46:59 -0400

    alteracao tela turma

--- StrictSensu/obj/Debug/net9.0/StrictSensu.AssemblyInfo.cs
+++ StrictSensu/obj/Debug/net9.0/StrictSensu.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StrictSensu")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c4230d839020689d79e848bcf9db46442c568b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4f54de255c765b766b4866ddacb2e0ec77aca84")]
 [assembly: System.Reflection.AssemblyProductAttribute("StrictSensu")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StrictSensu")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
